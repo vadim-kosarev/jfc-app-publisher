ca8a29e833ce8f8cc0e94cbc942dd85eed3de69a
Vadim Kosarev <vadim.kosarev@gmail.com>
2024-04-08 17:21:56 +0300

    Initial commit

---
+++ src/main/java/dev/vk/jfc/jfcapppublisher/JfcAppPublisherApplication.java
@@ -0,0 +1,13 @@
+package dev.vk.jfc.jfcapppublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfcAppPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfcAppPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vk/jfc/jfcapppublisher/JfcAppPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vk.jfc.jfcapppublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfcAppPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
